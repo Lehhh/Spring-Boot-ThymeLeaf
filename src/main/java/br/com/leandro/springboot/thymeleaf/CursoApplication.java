1bf8ef64de79d08c9b28a7128bb3cd30b8e168a2
Leandro Shiniti Tacara <leandro.tacara@hotmail.com>
2020-06-12 13:35:45 -0300

    Add Profiles

---
+++ src/main/java/br/com/leandro/springboot/thymeleaf/CursoApplication.java
@@ -0,0 +1,22 @@
+package br.com.leandro.springboot.thymeleaf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication implements CommandLineRunner {
+
+	@Value("${env}")
+	String test;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(test);
+	}
+}
